03421be2419a809ec05bf28d08600a004b4a8e31
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2024-07-24 23:11:39 +0530

    input validation fix

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -11,7 +11,7 @@ function GuessInput({handleSubmitGuesses,gameStatus}) {
   return (
     <form  onSubmit={handleSubmit} className="guess-input-wrapper">
       <label htmlFor="guess-input">Enter guess:</label>
-      <input disabled={gameStatus !== 'running'} required minLength={5} maxLength={5}  id="guess-input" type="text" value={guess} onChange={(event)=>{
+      <input disabled={gameStatus !== 'running'} minlength="5" maxlength="5" pattern=".{5,}"   id="guess-input" type="text" value={guess} onChange={(event)=>{
         setGuess(event.target.value.toUpperCase())
       }} />
     </form>
